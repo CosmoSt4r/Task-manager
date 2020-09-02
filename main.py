c44056b914d764eef103d35e73dfec968684cf9c
Andrey Kalkutin <cosmostar@protonmail.com>
2020-09-02 14:18:49 +0500

    Delete/Update buttons functionality added

--- main.py
+++ main.py
@@ -23,6 +23,33 @@ def index():
         return render_template('index.html', tasks=tasks)
 
 
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    task_to_delete = Task.query.get_or_404(task_id)
+
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/update/<int:task_id>', methods=['GET', 'POST'])
+def update(task_id):
+    task = Task.query.get_or_404(task_id)
+
+    if request.method == 'POST':
+        # POST
+
+        task.content = request.form['content']
+
+        db.session.commit()
+        return redirect('/')
+
+    else:
+        # GET
+
+        return render_template('update.html', task=task)
+
+
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
